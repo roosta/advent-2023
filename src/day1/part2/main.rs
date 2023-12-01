3bbf92915bb76915df2989133884e694d6a91678
Daniel Berg <mail@roosta.sh>
2023-12-01 16:28:28 +0100

    day1/part2: init

--- src/day1/part2/main.rs
+++ src/day1/part2/main.rs
@@ -1,3 +1,15 @@
+use std::fs;
+
 pub fn main() {
-    println!("hello part 2!");
+    let input = fs::read_to_string("input/day1/part2.txt").unwrap();
+    let result: Vec<i32> = input.lines().map(|line| {
+        // let left = line.find(|c: char| c.is_digit(10)).unwrap();
+        // let right = line.rfind(|c: char| c.is_digit(10)).unwrap();
+        // let mut p = String::new();
+        // p.push(line.chars().nth(left).unwrap());
+        // p.push(line.chars().nth(right).unwrap());
+        // let n: i32 = p.parse().unwrap();
+        12
+    }).collect();
+    println!("result = {:#?}", result);
 }
