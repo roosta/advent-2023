2c36cdf4e32b207db98530a85c6857cd3c71f581
Daniel Berg <mail@roosta.sh>
2023-12-01 16:24:22 +0100

    day1/part1: minor nitpicks

--- src/day1/part1/main.rs
+++ src/day1/part1/main.rs
@@ -3,14 +3,14 @@ use std::fs;
 pub fn main() {
     let input = fs::read_to_string("input/day1/part1.txt").unwrap();
     let result: i32 = input.lines().map(|line| {
-        let left = line.find(|c: char| c.is_digit(10)).expect("no left number found!");
-        let right = line.rfind(|c: char| c.is_digit(10)).expect("no right number found!");
+        let left = line.find(|c: char| c.is_digit(10)).unwrap();
+        let right = line.rfind(|c: char| c.is_digit(10)).unwrap();
         let mut p = String::new();
         p.push(line.chars().nth(left).unwrap());
         p.push(line.chars().nth(right).unwrap());
         let n: i32 = p.parse().unwrap();
         n
-    }).reduce(|a, b| a + b).unwrap();
+    }).reduce(|acc, n| acc + n).unwrap();
 
     println!("result = {}", result);
 }
