21180bb43b8fbf17d76d53bf028d2d769deff6d8
Daniel Berg <mail@roosta.sh>
2023-12-02 12:13:16 +0100

    day1/part2: solve for example input
    When running on actual data, it panics because it can't find both digits
    in some lines, which can't be right...shouldn't each line contain two
    digits?

--- src/day1/part2/main.rs
+++ src/day1/part2/main.rs
@@ -1,15 +1,106 @@
 use std::fs;
 
+#[derive(Debug)]
+struct Number {
+    i: i32,
+    alpha: char,
+    word: &'static str,
+}
+
+const NUM: [Number; 9] = [
+    Number { i: 1, alpha: '1', word: "one" },
+    Number { i: 2, alpha: '2', word: "two" },
+    Number { i: 3, alpha: '3', word: "three" },
+    Number { i: 4, alpha: '4', word: "four" },
+    Number { i: 5, alpha: '5', word: "five" },
+    Number { i: 6, alpha: '6', word: "six" },
+    Number { i: 7, alpha: '7', word: "seven" },
+    Number { i: 8, alpha: '8', word: "eight" },
+    Number { i: 9, alpha: '9', word: "nine" },
+];
+
+fn find_digit(line: &str) -> Option<Number> {
+    let mut pos = line.len();
+    let mut out: Option<Number> = None;
+    for n in NUM {
+        let word = line.find(n.word);
+        let alpha = line.find(n.alpha);
+        match (alpha, word) {
+            (Some(a), Some(w)) => {
+                if a < w && pos < a {
+                    pos = a;
+                    out = Some(n);
+                } else if w < a && pos < w {
+                    pos = w;
+                    out = Some(n);
+                }
+            },
+            (Some(a), None) => {
+                if a < pos {
+                    pos = a;
+                    out = Some(n)
+
+                }
+            },
+            (None, Some(w)) => {
+                if w < pos {
+                    pos = w;
+                    out = Some(n);
+                }
+            },
+            (None, None) => continue
+        };
+    }
+    out
+}
+
+fn rfind_digit(line: &str) -> Option<Number> {
+    let mut pos = 0;
+    let mut out: Option<Number> = None;
+    for n in NUM {
+        let word = line.rfind(n.word);
+        let alpha = line.rfind(n.alpha);
+        match (alpha, word) {
+            (Some(a), Some(w)) => {
+                if a > w && pos > a {
+                    pos = a;
+                    out = Some(n);
+                } else if w > a && pos > w {
+                    pos = w;
+                    out = Some(n);
+                }
+            },
+            (Some(a), None) => {
+                if a > pos {
+                    pos = a;
+                    out = Some(n)
+
+                }
+            },
+            (None, Some(w)) => {
+                if w > pos {
+                    pos = w;
+                    out = Some(n);
+                }
+            },
+            (None, None) => continue
+        };
+    }
+    out
+}
 pub fn main() {
     let input = fs::read_to_string("input/day1/part2.txt").unwrap();
-    let result: Vec<i32> = input.lines().map(|line| {
-        // let left = line.find(|c: char| c.is_digit(10)).unwrap();
-        // let right = line.rfind(|c: char| c.is_digit(10)).unwrap();
-        // let mut p = String::new();
-        // p.push(line.chars().nth(left).unwrap());
-        // p.push(line.chars().nth(right).unwrap());
-        // let n: i32 = p.parse().unwrap();
-        12
-    }).collect();
-    println!("result = {:#?}", result);
+    let result: Option<i32> = input.lines().map(|line| {
+        let left = match find_digit(line) {
+            Some(d) => d,
+            None => panic!("Can't find left digit in line: {}", line)
+        };
+        let right = match rfind_digit(line) {
+            Some(d) => d,
+            None => panic!("Can't find right digit in line: {}", line)
+
+        };
+        return left.i * 10 + right.i
+    }).reduce(|acc, n| acc + n);
+    println!("result = {:#?}", result.unwrap());
 }
