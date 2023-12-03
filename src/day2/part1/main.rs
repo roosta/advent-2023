76acfe4fcf1080dfc78db42d30fc3fb5ebc8134b
Daniel Berg <mail@roosta.sh>
2023-12-03 12:35:52 +0100

    day2/part1: init

---
+++ src/day2/part1/main.rs
@@ -0,0 +1,9 @@
+use std::fs;
+
+pub fn main() {
+    let input = fs::read_to_string("input/day1.txt").unwrap();
+    let mut result = 0;
+    for line in input.lines() {
+    }
+    println!("result = {:#?}", result);
+}
