7dae7533ee4a25b90f38e3e343699ba72bbbc499
Daniel Berg <mail@roosta.sh>
2023-12-17 16:21:50 +0100

    day6/part1: add solve

--- src/day6/part1/main.rs
+++ src/day6/part1/main.rs
@@ -1,5 +1,6 @@
 use std::fs;
 use advent_2023::parse_line;
+use std::iter::zip;
 // use std::collections::HashMap;
 
 #[derive(Debug)]
@@ -19,7 +20,26 @@ fn parse_game(input: String) -> Game {
 
 }
 
+fn run_opts(max_time: i64, record_distance: i64) -> i64  {
+    let mut nwins = 0;
+    for speed in 0..=max_time {
+        let time = max_time - speed;
+        let factor = speed % max_time;
+        let distance = time * factor;
+        if distance > record_distance {
+            nwins += 1;
+        }
+    }
+    nwins
+}
+
 fn main() {
     let input = fs::read_to_string("input/day6.txt").unwrap();
     let game = parse_game(input);
+    let iter = zip(game.time, game.distance);
+    let mut result = 1;
+    for (time, distance) in iter {
+        result *= run_opts(time, distance);
+    }
+    println!("Result = {}", result);
 }
