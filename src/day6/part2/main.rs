e28950484c22d8fe3591d1c086699ec783f233e0
Daniel Berg <mail@roosta.sh>
2023-12-17 16:26:19 +0100

    day6/part2: add solve
    Brute force solution, needs optimization but does finish reasonably
    quickly, enough to move on.

---
+++ src/day6/part2/main.rs
@@ -0,0 +1,45 @@
+use std::fs;
+// use advent_2023::parse_line;
+use std::iter::zip;
+
+#[derive(Debug)]
+struct Game {
+    time: i64,
+    distance: i64
+}
+
+fn parse_line(line: &str) -> i64 {
+    let alpha = line.replace(" ", "");
+    alpha.parse().unwrap()
+}
+
+fn parse_game(input: String) -> Game {
+    let mut iter = input.lines();
+    let time_str = iter.next().unwrap().split(':').last().unwrap();
+    let distance_str = iter.next().unwrap().split(':').last().unwrap();
+    return Game {
+        time: parse_line(time_str),
+        distance: parse_line(distance_str)
+    }
+
+}
+
+fn run_opts(max_time: i64, record_distance: i64) -> i64  {
+    let mut nwins = 0;
+    for speed in 0..=max_time {
+        let time = max_time - speed;
+        let factor = speed % max_time;
+        let distance = time * factor;
+        if distance > record_distance {
+            nwins += 1;
+        }
+    }
+    nwins
+}
+
+fn main() {
+    let input = fs::read_to_string("input/day6.txt").unwrap();
+    let game = parse_game(input);
+    let result = run_opts(game.time, game.distance);
+    println!("Result = {:#?}", result);
+}
