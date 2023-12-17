2b047a6b195ce67bb7cc8b95490936f00f77902b
Daniel Berg <mail@roosta.sh>
2023-12-17 15:42:16 +0100

    day6/part1: parse input

--- src/day6/part1/main.rs
+++ src/day6/part1/main.rs
@@ -1,5 +1,25 @@
 use std::fs;
-use std::collections::HashMap;
+use advent_2023::parse_line;
+// use std::collections::HashMap;
+
+#[derive(Debug)]
+struct Game {
+    time: Vec<i64>,
+    distance: Vec<i64>
+}
+
+fn parse_game(input: String) -> Game {
+    let mut iter = input.lines();
+    let time_str = iter.next().unwrap().split(':').last().unwrap();
+    let distance_str = iter.next().unwrap().split(':').last().unwrap();
+    return Game {
+        time: parse_line(time_str),
+        distance: parse_line(distance_str)
+    }
+
+}
 
 fn main() {
+    let input = fs::read_to_string("input/day6.txt").unwrap();
+    let game = parse_game(input);
 }
